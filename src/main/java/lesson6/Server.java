fd3caf9c7472d95436d788af6de0af6f533f48e6
OlegDmitrievich88 <70801999+OlegDmitrievich88@users.noreply.github.com>
2020-12-04 11:26:32 +0300

    commit

---
+++ src/main/java/lesson6/Server.java
@@ -0,0 +1,97 @@
+package lesson6;
+
+ackage ru.geekbrains.chat.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+    private static final Logger logger = Logger.getLogger("");
+    public Server() {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8189);
+            logger.log(Level.INFO,"Сервер запущен. Ожидаем клиентов...");
+            // System.out.println("Сервер запущен. Ожидаем клиентов...");
+            while (true) {
+                socket = server.accept();
+                logger.log(Level.INFO,"Клиент подключился");
+                // System.out.println("Клиент подключился");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void sendPersonalMsg(ClientHandler from, String nickTo, String msg) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nickTo)) {
+                o.sendMsg("from " + from.getNick() + ": " + msg);
+                from.sendMsg("to " + nickTo + ": " + msg);
+                return;
+            }
+        }
+        logger.log(Level.INFO,"Клиент с ником " + nickTo + " не найден в чате");
+        from.sendMsg("Клиент с ником " + nickTo + " не найден в чате");
+    }
+
+    public void broadcastMsg(ClientHandler from, String msg) {
+        for (ClientHandler o : clients) {
+            if (!o.checkBlackList(from.getNick())) {
+                o.sendMsg(msg);
+            }
+        }
+    }
+
+    public boolean isNickBusy(String nick) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nick)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadcastClientsList() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/clientslist ");
+        for (ClientHandler o : clients) {
+            sb.append(o.getNick() + " ");
+        }
+        String out = sb.toString();
+        for (ClientHandler o : clients) {
+            o.sendMsg(out);
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+        broadcastClientsList();
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+        broadcastClientsList();
+    }
+}
